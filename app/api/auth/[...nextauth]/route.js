6ffa1f5665ef9e5dabdd95ca43046459ebeed3e1
Nasimul650 <Nasimulnasim14@gmail.com>
2023-11-01 10:48:52 +0600

    2nd commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
-import FacebookProvider from 'next-auth/providers/facebook'
+import GitHubProvider from 'next-auth/providers/github' 
 import { connectToDB } from '@utils/database'
 import User from "@models/user";
 
@@ -10,10 +10,10 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-        })
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+          })
     ],
     callbacks: {
         async session({ session }) {
